8c4572a2c5acae2ee08413b728953bc4abc8ded7
Jerry Warner <jerrywarner.techgeek@gmail.com>
2018-12-30 17:24:57 -0600

    first commit

--- game_ai.py
+++ game_ai.py
@@ -1,46 +1,3 @@
-
-# GLOBAL VARS
-board_layout = [["_" for x in range(7)] for x in range(6)]
-column_str = "1  2  3  4  5  6  7"
-turn = "Player 2"
-game_won = False
-###
-
-def print_board():
-    # prints the current board
-    print(column_str)
-    for row in board_layout:
-        print('  '.join(row))
-    print("\n")
-
-
-def verify_drop_input():
-    # verify a legitimate player input for the piece
-    pass # CREATE LATER
-
-def drop_piece():
-    # gets player input and drops piece. then switches turn.
-    print(turn + "'s Turn")
-    player_column_input = input("Choose a column: ")
-    player_column_integer_input = int(player_column_input) - 1
-    print("You chose: " + player_column_input + "\n\n")
-    for i in range(5, -1, -1):
-        if board_layout[i][player_column_integer_input] == "_":
-            if turn == "Player 1":
-                board_layout[i][player_column_integer_input] = "X"
-                break
-            elif turn == "Player 2":
-                board_layout[i][player_column_integer_input] = "0"
-                break
-
-def change_turn():
-    global turn
-    if turn == "Player 1":
-        turn = "Player 2"
-    elif turn == "Player 2":
-        turn = "Player 1"
-
-
 def test_horiz(test_val):
     # _
     # return 1 if won
@@ -129,55 +86,72 @@ def test_back_diag(test_val):
             except IndexError:
                 break
 
-def game_won_test():
-    # test if four in a row
-    # do this by testing _ | / \
-    global game_won
-    test_val = ""
-    if turn == "Player 1":
-        test_val = "X"
-    elif turn == "Player 2":
-        test_val = "0"
-    # tests return 1 if game won, or 0 if not-won
+
+
+#########################################################
+# API for machine learning
+
+def apply_action(turn, board, action):
+    # update the board after applying the action
+    # turn is int representing player 1 or 2
+    # board is list
+    # action is int representing column (0 - 5)
+    for i in range(5, -1, -1):
+        if board[i][action] == 0:
+            if turn == 1:
+                board[i][action] = 1
+                break
+            elif turn == 2:
+                board[i][action] = 2
+                break
+
+
+def get_actions(board):
+    # return a list of possible actions
+    avail_actions = []
+    for i in range(7):
+        if board[0][i] == 0:
+            avail_actions.append(i)
+    return avail_actions
+
+
+def detect_winner(board, turn):
+    # return 1 or 2 if either of these players won, or return None
+    # turn == player turn, but also the value for that player on the board
     # test _
-    if test_horiz(test_val):
-        game_won = True
+    if test_horiz(turn):
+        return turn
     # test |
-    if test_vert(test_val):
-        game_won = True
+    if test_vert(turn):
+        return turn
     # test /
-    if test_forward_diag(test_val):
-        game_won = True
+    if test_forward_diag(turn):
+        return turn
     # test \
-    if test_back_diag(test_val):
-        game_won = True
+    if test_back_diag(turn):
+        return turn
+    return None
 
-def play_game():
-    # iterates through ... until game won
-    # 1. print the board
-    # 2. player input and drop piece
-    # 3. test if game won
-    # 4. switch turn
+def initialize_board():
+    # create a new board and return it
+    return [[0 for x in range(7)] for x in range(6)]
+
+def play_game_ai():
+    # functions available to you: load_model() and get_model_action(model, board)
+    # initialize everything
+    board = initialize_board()
+    turn = 1
+    model = load_model()
+    game_won = False
+    # play the game!
     while not game_won:
-        change_turn()
-        print_board()
-        drop_piece()
-        game_won_test()
-    print_board()
-    if "_" not in board_layout:
-        print("TIE GAME.")
-        return 0
-    print(turn.upper() + " WINS THE GAME!!!")
-
-
-
-
-
-# run functions / play the game
-play_game()
-
-
-
-
-
-
+        avail_actions = get_actions(board)
+        action = get_model_action(model, board)
+        apply_action(turn, board, action)
+        if detect_winner(board, turn):
+            return turn
+        else:
+            if turn == 1:
+                turn = 2
+            else:
+                turn = 1
